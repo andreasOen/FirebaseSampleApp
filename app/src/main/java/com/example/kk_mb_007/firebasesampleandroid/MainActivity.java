c67c561947c06a3fbcfc10aa6a82d624de059924
Andreas <andreas.oentoro@kaskusnetworks.com>
2017-07-18 13:00:03 +0700

    first push

---
+++ app/src/main/java/com/example/kk_mb_007/firebasesampleandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kk_mb_007.firebasesampleandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
